c16b28f92060f1dbdf955262a3eac676faf71c72
ConeConundrum <arkhsergej@gmail.com>
2019-03-26 21:49:10 +0300

    make basic view for main page

--- emitent/views.py
+++ emitent/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Emitent, Industry, IndustrySubgroup
 
-# Create your views here.
+
+# Render information for main page
+def main_page(request):
+    # Get basic emitent info, ready to publish
+    # TODO: Card contains NAME, INDUSTRY, SUB INDUSTRY, and processed CHARTS
+    # TODO: Need to add them in query sets, and filter by some criteria
+    emitents_card = Emitent.objects.filter(publish_stage__contains='3')
+    return render(request, 'emitent/main.html', {'cards': emitents_card})
